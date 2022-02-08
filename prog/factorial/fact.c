4d92c703899272dc408fa49459fb9e18062a2b38
ubuntu <ubuntu@ubuntu-VirtualBox>
2022-02-08 10:59:10 -0500

    fact

---
+++ prog/factorial/fact.c
@@ -0,0 +1,26 @@
+/*
+ * fact.c
+ *
+ *  Created on: Feb 8, 2022
+ *      Author: ubuntu
+ */
+#include <stdio.h>
+
+int fact(int n)
+{
+	if(n==0)
+		return 1;
+
+	return n*fact(n-1);
+}
+
+int main()
+{
+	int a;
+
+	a=fact(4);
+
+	printf("%d",a);
+
+	return 0;
+}
