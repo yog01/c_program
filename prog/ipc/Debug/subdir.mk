58cdd9ee371bf365874aa16719b4ae2713b81970
yog01 <yogitsingh16@gmail.com>
2024-06-06 00:27:02 +0530

    shared memory

--- prog/ipc/Debug/subdir.mk
+++ prog/ipc/Debug/subdir.mk
@@ -7,21 +7,28 @@ C_SRCS += \
 ../client.c \
 ../server.c 
 
-OBJS += \
-./client.o \
-./server.o 
-
 C_DEPS += \
 ./client.d \
 ./server.d 
 
+OBJS += \
+./client.o \
+./server.o 
+
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.c
+%.o: ../%.c subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
+
+clean: clean--2e-
+
+clean--2e-:
+	-$(RM) ./client.d ./client.o ./server.d ./server.o
+
+.PHONY: clean--2e-
 
